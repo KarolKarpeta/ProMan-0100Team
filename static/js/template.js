bd537bcb9ecc3583eae0ac5ee3d14a156d2b650a
BarbaraBochinska <bachna22@gmail.com>
2018-10-10 23:37:20 +0200

    created function that returns literal string with modal

--- static/js/template.js
+++ static/js/template.js
@@ -21,9 +21,29 @@ let templates = {
                 </div>
         
             </div> `
+        },
+
+        getModal: function() {
+            return  `
+                    <div id="modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Write your card title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">
+                            <input class="form-control" id="user-input" name="input" type="text">
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-primary">Add card</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>`
         }
-
-
 
 };
 
