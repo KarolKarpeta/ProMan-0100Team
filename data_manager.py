f86f2bec74644271fa6ac268d93b6642bf185e6e
Kamil Woś <inz.kamil.wos@gmail.com>
2018-10-24 02:43:12 +0200

    Add data_manager queries, to showBoards, showCards, update status

--- data_manager.py
+++ data_manager.py
@@ -6,3 +6,23 @@ def get_all_users(cursor):
     cursor.execute("""SELECT * FROM users; """)
     all_users = cursor.fetchall()
     return all_users
+
+
+@database_common.connection_handler
+def get_all_boards(cursor):
+    cursor.execute("""SELECT * FROM boards; """)
+    all_boards = cursor.fetchall()
+    return all_boards
+
+
+@database_common.connection_handler
+def get_cards_by_board_id(cursor, boardId ):
+    cursor.execute('SELECT * FROM cards WHERE board_id = {};'.format(boardId))
+    cards = cursor.fetchall()
+    return cards
+
+
+@database_common.connection_handler
+def update_status(cursor, cardId, newStatus, newBoardId):
+    cursor.execute(' UPDATE cards SET status_id = {}, board_id = {} '
+                    'WHERE id = %s;'.format(newStatus, newBoardId), (cardId,))
