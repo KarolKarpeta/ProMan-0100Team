5da66cdb7b42cad641e8fe3ef19977bc323c2e55
Kamil Woś <inz.kamil.wos@gmail.com>
2018-10-24 02:44:48 +0200

    Add new route to serwer , to handel reequest from AJAX

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import data_manager
 app = Flask(__name__)
 
@@ -9,6 +9,29 @@ def boards():
 
     print("USERS", data_manager.get_all_users())
     return render_template('boards.html')
+
+
+
+@app.route("/get-boards")
+def get_boards():
+    boards = data_manager.get_all_boards()
+    return jsonify(boards)
+
+@app.route("/get-cards-board-id/<int:boardId>")
+def get_cards_by_board_id(boardId):
+    cards = data_manager.get_cards_by_board_id(boardId)
+    return jsonify(cards)
+
+@app.route("/update-status", methods=["POST"])
+def update_status():
+    print(request.form)
+    cardId = request.form['cardId']
+    newStatus = request.form['newStatus']
+    newBoardId = request.form['newBoardId']
+
+    data_manager.update_status(cardId, newStatus, newBoardId)
+
+    return "success"
 
 
 def main():
