5f584d9cf31c5564edfb8c8791ea31a078adcac7
Karol Karpeta <karol.karpeta@gmail.com>
2018-10-24 13:04:12 +0200

    database_common variables defined

--- database_common.py
+++ database_common.py
@@ -5,10 +5,10 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
+    user_name = os.environ.get('PSQL_USER_NAME', 'student')
+    password = os.environ.get('PSQL_PASSWORD', 'codecool')
+    host = os.environ.get('PSQL_HOST', 'localhost')
+    database_name = os.environ.get('PSQL_DB_NAME', 'student')
 
     env_variables_defined = user_name and password and host and database_name
 
