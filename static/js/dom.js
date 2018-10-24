4b034d4eeb8119e97c94ee0c4997cd3602ba1899
BarbaraBochinska <bachna22@gmail.com>
2018-10-24 14:58:41 +0200

    Fixed function addNewBoard - (still have to refresh page) and did some refactor

--- static/js/dom.js
+++ static/js/dom.js
@@ -30,23 +30,24 @@ let dom = {
         })
     },
 
-    addNewBoard: function(){ // event handler function
+    addNewBoard: function() { // event handler function
             let title = document.getElementById("newBoardTitle").value;
-
-            if(title === ""){
+            if(title === "") {
                 alert("PLEASE FILL THE BOARD NAME!!!")
             }
             else {
-
             let xhr = new XMLHttpRequest();
-            xhr.open('POST', 'http://127.0.0.1:5000//add-board', true);
+            xhr.open('POST', 'http://127.0.0.1:5000/add-board', true);
             xhr.responseText = 'text';
-
-            xhr.onload = function () {
-                let board = JSON.parse(xhr.response);
-            }
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function() {//Call a function when the state changes.
+                if(xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText);
+                }
+            };
+            xhr.send(`title=${title}`);
         }
-        xhr.send();
+
     },
 
     addNewCard: function() {
@@ -91,13 +92,11 @@ let dom = {
 
                     let newCardButton = $(`#new-card-${board.id}`);
                     newCardButton.click(function (ev){
-                        console.log(ev.target);
                         let modal = document.getElementById("modal");
                         modal.dataset.boardId = board.id;
                     });
 
                     let boardAnchor = document.getElementById(`heading-${board.id}`);
-                    console.log("test");
                     boardAnchor.addEventListener('click', dom.showCards);
                     dom.showCardsCounter(board.id);
                 }
@@ -139,7 +138,6 @@ let dom = {
     drop: function(dropevent, newStatus, newBoardId) {
         dropevent.preventDefault();
         var cardId = dropevent.dataTransfer.getData("element-id");
-        console.log("CardId",parseInt(cardId), cardId );
         cardId = parseInt(cardId);
 
 
@@ -155,17 +153,11 @@ let dom = {
 
 
             xhr.onload = function () {
-                console.log(xhr.response);
-
-
                 dropevent.target.appendChild(document.getElementById(cardId));
                 dom.showCardsCounter(newBoardId);
             };
             xhr.send(`cardId=${cardId}&newStatus=${newStatus}&newBoardId=${newBoardId}`);
-
-
-
-        dataHandler.modifyStatus(cardId, newStatus, newBoardId);
+            dataHandler.modifyStatus(cardId, newStatus, newBoardId);
 
         },
 
@@ -173,42 +165,31 @@ let dom = {
     showCards: function(event) {
         // shows the cards of a board
         // it adds necessary event listeners also
-        boardId = parseInt(event.target.dataset.boardId);
-        console.log("boardId", boardId);
-
+        let boardId = parseInt(event.target.dataset.boardId);
         let xhr = new XMLHttpRequest();
         xhr.open('GET', `http://127.0.0.1:5000/get-cards-board-id/${boardId}`, true);
         xhr.responseText = 'text';
 
 
         xhr.onload = function () {
-            cards = JSON.parse(xhr.response);
-            console.log("Cards", cards);
+            let cards = JSON.parse(xhr.response);
             if (cards.length) {
                 //let board = document.getElementById(`collapseBoard-${cards[0].board_id}`);
-
                 for (let stat = 1; stat <= 4; stat++) {
-                    div = document.getElementById(`board-${cards[0].board_id}-status-${stat}`);
-                    div.innerText = "";
+                   let div = document.getElementById(`board-${cards[0].board_id}-status-${stat}`);
+                   div.innerText = "";
                 }
-
-                // optymalize!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                //
+                // ~~~~~~~~~~~~~~ optymalize this code ~~~~~~~~~~~~~~
                 for (let card of cards) {
                     let statusDiv = document.getElementById(`board-${card.board_id}-status-${card.status_id}`);
-
                     let el = document.createElement("div");
-
                     el.setAttribute('id', card.id);
                     el.setAttribute('status', card.status_id);
-
                     el.setAttribute('draggable', 'true');
                     el.setAttribute('ondragstart', 'dom.drag(event)');
-
                     el.innerText = card.title;
                     el.classList.add("border");
                     el.classList.add("text-center");
-
                     statusDiv.appendChild(el);
                 }
                 dom.showCardsCounter(cards[0].board_id);
@@ -224,15 +205,10 @@ let dom = {
         xhr.responseText = 'text';
 
         xhr.onload = function () {
-            counter = JSON.parse(xhr.response);
-            console.log("Counter", counter);
-
+            let counter = JSON.parse(xhr.response);
             let cardsCounterBadge = document.querySelector(`#header-${boardId} .badge`);
             cardsCounterBadge.innerText = 777;
-
         };
-
-
         xhr.send();
 
     }
